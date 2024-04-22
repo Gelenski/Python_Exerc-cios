aa28c1e0f448f59d84ee7c476d058bf59f01bddc
Lucas Gelenski <gelenskilucas@gmail.com>
2024-04-22 14:26:04 -0300

    finalizando atividade 4

--- Python4.py
+++ Python4.py
@@ -3,6 +3,10 @@ nums = [10, 4, 5, 3, 6, 7, 8, 2, 9, 1]
 
 
 def lista_nums():
+    maior_numero = max(nums)
+    for index in range(maior_numero, 0, -1):
+        if index in nums:
+            print(index)
 
 
 lista_nums()
