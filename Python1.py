527f584040b62dd14bff68c6b8f7c7beedd584e7
Lucas Gelenski <gelenskilucas@gmail.com>
2024-04-22 09:05:59 -0300

    realizando as primeiras 2 atividades

---
+++ Python1.py
@@ -0,0 +1,6 @@
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+names = ["Lucas", "Joâo", "Ricardo", "Cleverson", "Ziraldo"]
+year = [2007, 2024]
+print(nums)
+print(names)
+print(year)
---
+++ Python2.py
@@ -0,0 +1,10 @@
+# Crie uma lista e utilize um loop for para percorrer todos os elementos da lista.
+names = ["Lucas", "Joâo", "Ricardo", "Cleverson", "Ziraldo"]
+
+
+def lista_nomes():
+    for index in names:
+        print(index)
+
+
+lista_nomes()
