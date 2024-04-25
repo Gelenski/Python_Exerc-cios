db6e5dc7cbd2373ae8fe9ac5e31d46ef96e933a8
Lucas Gelenski <gelenskilucas@gmail.com>
2024-04-25 18:31:19 -0300

    Comentando python1

--- Python1.py
+++ Python1.py
@@ -1,3 +1,8 @@
+# Crie  uma  lista  para  cada  informação  a seguir:
+# -  Lista de números de 1 a 10
+# -  Lista com quatro nomes
+# -  Lista  com  o  ano  que  você  nasceu  e  o ano atual.
+
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 names = ["Lucas", "Joâo", "Ricardo", "Cleverson", "Ziraldo"]
 year = [2007, 2024]
